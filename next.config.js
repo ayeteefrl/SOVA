921ab1c27fb4b84895e86f41d13eab717be06403
Advik Teotea <advik163@gmail.com>
2026-05-03 00:11:56 +0530

    Fix kiteconnect bundling for Next.js 14

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ['kiteconnect'],
+  experimental: {
+    serverComponentsExternalPackages: ['kiteconnect'],
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
